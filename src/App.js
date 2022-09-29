8f4dd55f837cfea1d5503eaf375cd828ad976c6b
argha-mirinfo <argha@mirinfosys.com>
2022-09-29 19:26:01 +0600

    home page design on going

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar';
+import HomePage from './pages/homepage';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className='mt-28'>
+        <HomePage/>
+      </div>
     </div>
   );
 }
