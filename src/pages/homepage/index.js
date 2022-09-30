3455a08f4ffca7e8aeb8d068495939cf7c64b770
argha-mirinfo <argha@mirinfosys.com>
2022-09-30 13:07:06 +0600

    company about component done

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomePageBanner from '../../components/homepage-banner'
+import CompanyAbout from '../../components/company-about'
 
 const HomePage = () => {
   return (
     <div>
         <HomePageBanner/>
+        <CompanyAbout/>
     </div>
   )
 }
