51b8b6c1c9f4ffd1d642d5a3ed455c7fc19914e0
argha-shipansarker <arghasarker56@gmail.com>
2022-10-03 10:02:11 +0600

    donation page done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import HomePage from './pages/homepage';
+import DonationPage from './pages/donation';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path='/education-website' element={<HomePage />} />
+        <Route path='/donation' element={<DonationPage />} />
       </Routes>
     </div>
   );
